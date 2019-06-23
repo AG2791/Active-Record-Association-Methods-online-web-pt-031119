76348ceef7cbe803aaf0dbd260c67a31a65ecd32
AJ Guiles <ashiguiles@gmail.com>
2019-06-23 00:30:06 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,8 +4,13 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+<<<<<<< HEAD
     self.genres.songs.first 
    
+=======
+    genres.songs.first 
+    
+>>>>>>> d6f8f46436189fd6db55fd8fdf2e1e426d018cc0
   end
 
   def song_count
